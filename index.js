528bc54ad436874e59cf0df44e5a12203c5d5230
Joseph <josephputz89@gmail.com>
2022-05-17 17:11:32 -0500

    commit tree ffc31b129ab5875347be652c69f3a561b4d03db4

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+//function findMatching(drivers, name) {
+//    for (const user of array) {
+//        if (user === "string") {
+//            return user;
+//        }
+//    }
+//}
+
+function findMatching(drivers, name) {
+    let match = drivers.filter(driverName => driverName.toUpperCase() === name.toUpperCase());
+    return match;
+}
+
+
+function fuzzyMatch(drivers, name) {
+    let match = drivers.filter(driverName => driverName.slice(0, name.length) === name);
+    return match;
+}
+
+//function fuzzyMatch(drivers, name){
+//    const fuzzyDrivers = drivers.filter(function(person){
+//        return person.slice(0) == name.slice(0);
+//    });
+//    return fuzzyDrivers;
+//}
+
+//function matchName(drivers, name) {
+//    let match = drivers.filter(driverName.name => driverName.name === name);
+//    return match;
+//}
+
+function matchName(object, string) {
+    return object.filter(driver => driver.name === string);
+}
